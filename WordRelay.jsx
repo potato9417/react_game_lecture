3fba4eff280d56ff69e0946082cdd543ee27fefc
히지니 <potato9417@gmail.com>
2020-07-08 16:30:03 +0900

    react , webpack설치 및 기본 사용방법

---
+++ WordRelay.jsx
@@ -0,0 +1,13 @@
+const React = require("react")
+
+class Wordrelay extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
+
+module.exports=Wordrelay
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports={
+    name: "word-relay-setting",
+    mode: "development", // 실제 배포할때  production 으로 변경
+    devtool: "eval", // 속도빠르게 한다는 뜻
+
+    // 중요!
+    entry:{
+
+    }, // 입력
+    output:{
+        
+    } // 출력
+};
