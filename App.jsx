1ff6b80ee45b13494798c2ad9d516adf01fcc16c
히지니 <potato9417@gmail.com>
2020-07-09 14:52:21 +0900

    구구단 jsx로 변경후 병합

--- App.jsx
+++ App.jsx
@@ -3,14 +3,14 @@ const BrowserRouter = require("react-router-dom").BrowserRouter;
 const Route = require("react-router-dom").Route;
 
 const WordRelay = require("./WordRelay")
-// const Gugudan = 
+const Gugudan = require("./Gugudan")
 const Nav = require("./Nav")
 
 const App=()=>{
     return(
         <BrowserRouter>
             <Nav />
-            <Route path="/gugudan" component={WordRelay}/>
+            <Route path="/gugudan" component={Gugudan}/>
             <Route path="/word_relay" component={WordRelay}/>
         </BrowserRouter>
     )
