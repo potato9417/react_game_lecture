11293948fea68f2142aee08c6f3ff23919d0b502
히지니 <potato9417@gmail.com>
2020-07-14 12:56:29 +0900

    function component로 변경

--- NumberBaseball.jsx
+++ NumberBaseball.jsx
@@ -11,36 +11,25 @@ const getNum=()=>{
     // console.log(getNumArr)
     return getNumArr
 }
+ 
+const NumberBaseball=()=>{
+    const [number,setNumber]=React.useState(getNum())
+    const [value,setValue]=React.useState("")
+    const [tries,setTries]=React.useState([])
 
-class NumberBaseball extends React.Component{
-    state={
-        number:getNum(),
-        value:"",
-        tries:[]
+    const changeInput=(e)=>{
+        setValue(e.target.value)
     }
 
-    changeInput=(e)=>{
-        this.setState({value:e.target.value})
-    }
-
-    checkResult=(e)=>{
+    const checkResult=(e)=>{
         e.preventDefault();
-        const {number,value,result,tries}=this.state
         console.log("click")
         if(value===number.join("")){
-            this.setState({
-                value:"",
-                result:"홈런!",
-                tries:[...this.state.tries, {tryNum:value,result:"홈런!"}]
-                // 배열:[기존배열,{추가할 내용}]  기존배열은 ...으로 가지고있는 모든것을 가져와야한다  => push 대신 리액트에서 변화된 배열을 체크할수있게 사용
-            })
             alert("홈런! 새 게임을 다시 시작합니다.")
-            this.setState({
-                value:"",
-                number:getNum(),
-                tries:[]
-                // 배열:[기존배열,{추가할 내용}]  기존배열은 ...으로 가지고있는 모든것을 가져와야한다  => push 대신 리액트에서 변화된 배열을 체크할수있게 사용
-            })
+            
+            setValue("")
+            setNumber(getNum())
+            setTries([])
         }
         else {
             const answerArr = value.split("")
@@ -51,7 +40,10 @@ class NumberBaseball extends React.Component{
             // 10번이상 틀렸을 경우 
             if(tries.length>=3){
                 alert(`기회 10번 모두 사용하셨습니다. 답은 ${number.join(",")}`)
-                this.setState({number:getNum(),tries:[],value:""})
+
+                setNumber(getNum())
+                setTries([])
+                setValue("")
             }
             else{
                 for(let i=0;i<4;i++){
@@ -63,35 +55,34 @@ class NumberBaseball extends React.Component{
                         ball+=1
                     }
                 }
-                this.setState({
-                    tries:[...this.state.tries, {tryNum:value,result:`${strike}S, ${ball}B`}],value:""
-                })
+                setTries([...tries, {tryNum:value,result:`${strike}S, ${ball}B`}])
+                setValue("")
+                
             }
         }
     }
 
-    render(){
-        const {number,value,tries}=this.state
-        return (
-            <div>
-                <h2>숫자야구</h2>
-                <h3>네자리 숫자를 맞춰주세요 {number}</h3>
-                <input maxLength="4" type="number" value={value} onChange={this.changeInput}/>
-                <button onClick={this.checkResult}>확인</button>
-                <p>{tries.length}회 시도</p>
-                <ul>
-                    {tries.map((props,index)=>{
-                        return (
-                            <li key={index}>
-                                <p>{props.tryNum}</p>
-                                <p>{props.result}</p>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h2>숫자야구</h2>
+            <h3>네자리 숫자를 맞춰주세요 {number}</h3>
+            <input maxLength="4" type="number" value={value} onChange={changeInput}/>
+            <button onClick={checkResult}>확인</button>
+            <p>{tries.length}회 시도</p>
+            <ul>
+                {tries.map((props,index)=>{
+                    return (
+                        <li key={index}>
+                            <p>{props.tryNum}</p>
+                            <p>{props.result}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+    
+
 }
 
 module.exports=NumberBaseball
