24d9b5a00a1338b52d95b9a8a893d67dff379dd7
히지니 <potato9417@gmail.com>
2020-07-14 10:54:52 +0900

    오류 수정중.. 오류가 왜케 많이나..

--- NumberBaseball.jsx
+++ NumberBaseball.jsx
@@ -17,7 +17,6 @@ class NumberBaseball extends React.Component{
     state={
         number:getNum(),
         value:"",
-        result:"",
         tries:[]
     }
 
@@ -31,40 +30,48 @@ class NumberBaseball extends React.Component{
         console.log("click")
         if(value===number.join("")){
             this.setState({
+                value:"",
                 result:"홈런!",
                 tries:[...this.state.tries, {tryNum:value,result:"홈런!"}]
                 // 배열:[기존배열,{추가할 내용}]  기존배열은 ...으로 가지고있는 모든것을 가져와야한다  => push 대신 리액트에서 변화된 배열을 체크할수있게 사용
             })
-        }
-        else {
-            const answerArr = value.split("").map((props)=>{
-                parseInt(props)
+            alert("홈런! 새 게임을 다시 시작합니다.")
+            this.setState({
+                value:"",
+                number:getNum(),
+                tries:[]
+                // 배열:[기존배열,{추가할 내용}]  기존배열은 ...으로 가지고있는 모든것을 가져와야한다  => push 대신 리액트에서 변화된 배열을 체크할수있게 사용
             })
-            let strike=0
-            let ball=0
-
-            // 10번이상 틀렸을 경우 
-            if(tries.length>=9){
-                this.setState({
-                    result:`기회 10번 모두 사용하셨습니다. 답은 ${value.join(",")}`
-                })
-                alert("게임을 다시 시작합니다.")
-                this.setState({number:getNum(),tries:[],value:""})
-            }
-            else{
-                for(let i=0;i<4;i++){
-                    if(answerArr[i]===value[i]){
-                        strike+=1
-                    }
-                    else if(value.includes(answerArr[i])){
-                        ball+=1
-                    }
-                }
-                this.setState({
-                    tries:[...this,state,tries, {try:value,rsult:`${strike}스트라이크, ${ball}볼 입니다.`,value:""}]
-                })
-            }
         }
+        // else {
+        //     const answerArr = value.split("").map((props)=>{
+        //         parseInt(props)
+        //     })
+        //     let strike=0
+        //     let ball=0
+
+        //     // 10번이상 틀렸을 경우 
+        //     if(tries.length>=9){
+        //         this.setState({
+        //             result:`기회 10번 모두 사용하셨습니다. 답은 ${value.join(",")}`
+        //         })
+        //         alert("게임을 다시 시작합니다.")
+        //         this.setState({number:getNum(),tries:[],value:""})
+        //     }
+        //     else{
+        //         for(let i=0;i<4;i++){
+        //             if(answerArr[i]===value[i]){
+        //                 strike+=1
+        //             }
+        //             else if(value.includes(answerArr[i])){
+        //                 ball+=1
+        //             }
+        //         }
+        //         this.setState({
+        //             tries:[...this,state,tries, {tryNum:value,rsult:`${strike}스트라이크, ${ball}볼 입니다.`,value:""}]
+        //         })
+        //     }
+        // }
     }
 
     render(){
@@ -75,6 +82,7 @@ class NumberBaseball extends React.Component{
                 <h3>네자리 숫자를 맞춰주세요 {number}</h3>
                 <input maxLength={4} type="number" value={value} onChange={this.changeInput}/>
                 <button onClick={this.checkResult}>확인</button>
+                <p>{tries.length}회 시도</p>
                 <ul>
                     {tries.map((props,i)=>{
                         <Try key={`${i+1}차시도:`} tryInfo={props} />})}
