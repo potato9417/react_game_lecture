c03b8a097969328a98560a064740d57b3e63dd1d
히지니 <potato9417@gmail.com>
2020-07-15 11:06:08 +0900

    조건문

--- ResponseCheck.jsx
+++ ResponseCheck.jsx
@@ -3,17 +3,23 @@ const React = require("react")
 class ResponseCheck extends React.Component{
     state={
         state:"waiting",
-        message:"클릭해서 시작하세요."
+        message:"클릭해서 시작하세요.",
+        result:[]
+    }
+
+    changeScreen=()=>{
+        console.log("click")
     }
 
     render(){
-        const {message}=this.state
+        const {state,message,result}=this.state
         return(
             <div>
                 <h2>반응속도체크</h2>
-                <div id="screen" onClick={this.changeScreen}>
+                <div id="screen" className={state} onClick={this.changeScreen}>
                     <p>{message}</p>
                 </div>
+                {result.length !==0 && <h4>평균시간 : {result.reduce((a,c)=>a+c)/result.length}ms</h4>}
             </div>
         )
     }
