4650785341ebafa19c00f60c769a8bc21b6ddf2a
히지니 <potato9417@gmail.com>
2020-07-15 12:09:26 +0900

    function component로 변환

--- ResponseCheck.jsx
+++ ResponseCheck.jsx
@@ -1,76 +1,61 @@
 const React = require("react")
 
-class ResponseCheck extends React.Component{
-    state={
-        state:"waiting",
-        message:"클릭해서 시작하세요.",
-        result:[]
-    }
+const ResponseCheck=()=>{
+    const [state,setState]=React.useState("waiting")
+    const [message,setMessage]=React.useState("클릭해서 시작하세요.")
+    const [result,setResult]=React.useState([])
+    const timeout=React.useRef(null)
+    const startTime=React.useRef()
+    const endTime=React.useRef()
 
-    timeout;
-    startTime;
-    endTime;
-
-    changeScreen=()=>{
+    const changeScreen=()=>{
         console.log("click")
-        const {state,message,result}=this.state
 
         if(state==="waiting"){
-            this.setState({
-                state:"ready",
-                message:"초록색이 되면 클릭하세요."
-            })
-            this.timeout=setTimeout(() => {
-                this.setState({
-                    state:"now",
-                    message:"지금 클릭!"
-                })
-                this.startTime=new Date()
+            setState("ready")
+            setMessage("초록색이 되면 클릭하세요.")
+
+            timeout.current=setTimeout(() => {
+                setState("now")
+                setMessage("지금 클릭!")
+
+                startTime.current=new Date()
             }, Math.floor((Math.random()*1000)+2000));
         }
         else if(state==="ready"){
-            clearTimeout(this.timeout)
-            this.setState({
-                state:"waiting",
-                message:"너무 성급하시군요! 초록색이 된 후에 클릭하세요."
-            })
+            clearTimeout(timeout.current)
+
+            setState("waiting")
+            setMessage("너무 성급하시군요! 초록색이 된 후에 클릭하세요.")
         }
         else if(state==="now"){
-            this.endTime=new Date()
-            this.setState((prevState)=>{
-                return{
-                    state:"waiting",
-                    message:"클릭해서 시작하세요.",
-                    result:[...prevState.result,this.endTime-this.startTime]
-                }   
-            })
+            endTime.current=new Date()
+
+            setState("waiting")
+            setMessage("클릭해서 시작하세요.")
+            setResult((prevResult)=>{return[...prevResult,endTime.current-startTime.current]})
         }
     }
 
-    reset=()=>{
-        this.setState({
-            result:[]
-        })
+    const reset=()=>{
+        setResult([])
     }
 
-    render(){
-        const {state,message,result}=this.state
-        return(
-            <div>
-                <h2>반응속도체크</h2>
-                <div id="screen" className={state} onClick={this.changeScreen}>
-                    <p>{message}</p>
-                </div>
-                {result.length !==0 && 
-                    <div>
-                        <h4>반응속도 : {result[result.length-1]}ms</h4>
-                        <h4>평균시간 : {result.reduce((a,c)=>a+c)/result.length}ms</h4>
-                        <button onClick={this.reset}>초기화</button>
-                    </div>
-                }
+    return(
+        <div>
+            <h2>반응속도체크</h2>
+            <div id="screen" className={state} onClick={changeScreen}>
+                <p>{message}</p>
             </div>
-        )
-    }
+            {result.length !==0 && 
+                <div>
+                    <h4>반응속도 : {result[result.length-1]}ms</h4>
+                    <h4>평균시간 : {result.reduce((a,c)=>a+c)/result.length}ms</h4>
+                    <button onClick={reset}>초기화</button>
+                </div>
+            }
+        </div>
+    )
 }
 
 module.exports=ResponseCheck
