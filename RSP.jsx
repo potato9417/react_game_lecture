db7df0d8d9d52211d7dc21780f6143ff9b426cc9
히지니 <potato9417@gmail.com>
2020-07-15 16:07:40 +0900

    오류...

--- RSP.jsx
+++ RSP.jsx
@@ -6,16 +6,16 @@ const React = require("react")
 // 부모가 컴포넌트를 없앨 때-> componentWillUnmount -> 소멸
 
 const rspCoords={
-    바위:"0",
-    가위:"-142px",
-    보:"-284px"
+    rock:"0",
+    scissor:"-142px",
+    paper:"-284px"
 }
 
-const scores ={
-    바위:1,
-    가위:0,
-    보:-1
-}
+// const scores ={
+//     rock:1,
+//     scissor:0,
+//     paper:-1
+// }
 
 class RSP extends React.Component{
     state={
@@ -30,25 +30,18 @@ class RSP extends React.Component{
         const {imgCoord}=this.state
 
         this.interval=setInterval(()=>{
-
-            // console.log("hi")
-            if(imgCoord===rspCoords.바위){
+            console.log(imgCoord)
+            if(imgCoord===rspCoords.rock){
                 console.log("바위")
-                this.setState({
-                    imgCoord:rspCoords.가위
-                })
+                this.setState({imgCoord:rspCoords.scissor})
             }
-            else if(imgCoord===rspCoords.가위){
+            else if(imgCoord===rspCoords.scissor){
                 console.log("가위")
-                this.setState({
-                    imgCoord:rspCoords.보
-                })
+                this.setState({imgCoord:rspCoords.paper})
             }
-            else if(imgCoord===rspCoords.보){
+            else if(imgCoord===rspCoords.paper){
                 console.log("보")
-                this.setState({
-                    imgCoord:rspCoords.바위
-                })
+                this.setState({imgCoord:rspCoords.rock})
             }
         },500)
     }
@@ -60,19 +53,20 @@ class RSP extends React.Component{
 
     // 컴포넌트가 제거되기 직전 => 비동기 요청 정리 주로 함
     componentWillUnmount(){
-
+        clearInterval(this.interval)
     }
 
     render(){
         const {result,score,imgCoord}=this.state
+        console.log(imgCoord,"이거확인")
 
         return(
             <div>
                 <h2>가위바위보</h2>
-                <div id="computer" style={{background:`url(http://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}}></div>
+                <div id="computer" style={{background:`url(http://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}} />
                 <div>
-                    <button id="scissor">가위</button>
                     <button id="rock">바위</button>
+                    <button id="scissor">가위</button>
                     <button id="paper">보</button>
                 </div>
                 <div>
