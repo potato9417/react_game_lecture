80266a7d8633b032487b9ab91fcde23d68cebad7
히지니 <potato9417@gmail.com>
2020-07-15 13:18:12 +0900

    가위바위보 만드는 도중 오류발생.. 왜발생..?

--- RSP.jsx
+++ RSP.jsx
@@ -1,15 +1,56 @@
 const React = require("react")
 
+// 클래스의 LifeCycle
+// construncotr -> render -> ref -> componentDidMount -> 
+// (setState/props 바뀔때 -> shouldComponentUpdate -> render -> componentDidUpdate)
+// 부모가 컴포넌트를 없앨 때-> componentWillUnmount -> 소멸
+
+const rspCoords={
+    바위:"0",
+    가위:"-142px",
+    보:"-284px"
+}
+
+const scores ={
+    바위:1,
+    가위:0,
+    보:-1
+}
+
 class RSP extends React.Component{
     state={
         result:"",
         score:0,
-        imgCoord:0
+        imgCoord:"0"
     }
 
-    // 컴포넌트가 첫 렌더링 된 후
+    interval;
+    // 컴포넌트가 첫 렌더링 된 후 => 비동기 요청 주로 함
     componentDidMount(){
+        const {imgCoord}=this.state
 
+        this.interval=setInterval(()=>{
+
+            // console.log("hi")
+            if(imgCoord===rspCoords.바위){
+                console.log("바위")
+                this.setState({
+                    imgCoord:rspCoords.가위
+                })
+            }
+            else if(imgCoord===rspCoords.가위){
+                console.log("가위")
+                this.setState({
+                    imgCoord:rspCoords.보
+                })
+            }
+            else if(imgCoord===rspCoords.보){
+                console.log("보")
+                this.setState({
+                    imgCoord:rspCoords.바위
+                })
+            }
+        },500)
     }
 
     // 리렌더링 후 
@@ -17,7 +58,7 @@ class RSP extends React.Component{
 
     }
 
-    // 컴포넌트가 제거되기 직전
+    // 컴포넌트가 제거되기 직전 => 비동기 요청 정리 주로 함
     componentWillUnmount(){
 
     }
