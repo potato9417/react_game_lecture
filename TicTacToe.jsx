b1cfac2903d93fdb8b7a84688b4339a2c7100e8b
히지니 <potato9417@gmail.com>
2020-07-21 12:16:19 +0900

    useReducer사용 -> 이해안감,,,

--- TicTacToe.jsx
+++ TicTacToe.jsx
@@ -1,16 +1,37 @@
 const React =require("react")
 const Table = require("./Table")
 
+const initialState={
+    winner:"",
+    turn:"O",
+    tableDate:[["","",""],["","",""],["","",""]]
+}
+
+const reducer=(state,action)=>{
+    switch(action.type){
+        case "SET_WINNER":
+            return{
+                ...state,
+                winner:action.winner
+            }
+    }
+}
+
 const TicTacToe=()=>{
-    const [winner,setWinner]=React.useState("")
-    const [turn,setTurn]=React.useState("O")
-    const [tableDate,setTableDate]=useState([["","",""],["","",""],["","",""]])
+    const [state,dispatch]=React.useReducer(reducer,initialState); // useState의 갯수가 많아질때 useReducer로 하나로 사용가능
+    // const [winner,setWinner]=React.useState("")
+    // const [turn,setTurn]=React.useState("O")
+    // const [tableDate,setTableDate]=React.useState([["","",""],["","",""],["","",""]])
+
+    const tableClick=React.useCallback(()=>{
+        dispatch({type:"SET_WINNER",winner:"O"})
+    },[])
 
     return(
         <div>
             <h2>틱택토</h2>
             <Table />
-            {<p></p>}
+            {state.winner&&<p>{state.winner}님의 승리</p>}
         </div>
     )
 }
